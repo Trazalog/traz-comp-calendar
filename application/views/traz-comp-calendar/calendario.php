05fcd8561b70b3c5b25d82e70c0d3d53e5de2f87
Tincho Bustos <martingabriel.bustos@gmail.com>
2020-03-24 10:33:32 -0300

    jquery

--- application/views/traz-comp-calendar/calendario.php
+++ application/views/traz-comp-calendar/calendario.php
@@ -62,7 +62,8 @@
 <script src='lib/fullcalendar/core/locales/es.js'></script>
 <script src='lib/fullcalendar/bootstrap/main.js'></script>
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script> -->
 <script>
    // document.addEventListener('DOMContentLoaded', function() {
    // var calendarEl = $('#calendar');
